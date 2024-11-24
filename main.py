cbfea6c9f7a784fc5983cc69caa0f0860e025a6a
Douglas Saturnino <douglas02saturnino@gmail.com>
2024-11-24 16:12:23 -0300

    Adição de lógica para inserir informações do contato

---
+++ main.py
@@ -0,0 +1,15 @@
+names = []
+ages = []
+emails = []
+
+for i in range(3):
+    print(i + 1, "Input")
+    name = input("Name: ")
+    age = input("Age: ")
+    email = input("Email: ")
+
+names.append(name)
+ages.append(age)
+emails.append(email)
+
+print(names, ages, emails)
