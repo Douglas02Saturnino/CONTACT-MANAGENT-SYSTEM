dafb657bc2842d966fbd4001ca46762b47d84e7a
Douglas Saturnino <douglas02saturnino@gmail.com>
2024-11-24 16:24:03 -0300

    Função para realizar cadastro de informações do contato

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
-names = []
-ages = []
-emails = []
-
-for i in range(3):
-    print(i + 1, "Input")
+def add_person():
     name = input("Name: ")
     age = input("Age: ")
     email = input("Email: ")
 
-names.append(name)
-ages.append(age)
-emails.append(email)
+    person = {"name": name, "age": age, "email": email}
+    return person
 
-print(names, ages, emails)
+
+print("Hi, welcome to the Contact Managent System.")
+print()
+command = input("You can 'Add', 'Delete' or 'Search': ").upper()
+people = []
+
+if command == "ADD":
+    person = add_person()
+    people.append(person)
+    print("Person added.")
+elif command == "DELETE":
+    pass
+elif command == "SEARCH":
+    pass
+else:
+    print("Invalid command.")
+
+print(people)    
